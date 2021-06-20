5657e6cf0fe905ef04b083f97e5dba43965f12d9
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 23:51:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
   
   def new_appointment(date, doctor)
-    appt = Appointment.new(date, self, doctor)
+    appt = Appointment.new(date, doctor, self)
   end
   
   def appointments
