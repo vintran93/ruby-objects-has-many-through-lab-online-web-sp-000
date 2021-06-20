1af21bfab3a4e669871976f6e100d0975a98f448
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 23:22:05 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,28 @@
 class Genre 
   
-    attr_accessor :name
-    @@all = []
+  attr_accessor :name
     
-    def initialize(name)
-        @name = name
-        @@all << self
+  @@all = []
+    
+  def initialize(name)
+     @name = name
+     @@all << self
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def artists
+    Artist.all.select do |artist|
+      artist.genre = self
     end
+  end
 
-    def self.all
-        @@all
-
+  def songs
+    Song.all.select do |song|
+        song.genre = self
     end
-
-    def artists
-        Artist.all.select do |artist|
-            artist.genre = self
-        end
-
-    end
-
-    def songs
-        Song.all.select do |song|
-            song.genre = self
-        end
-    end
+  end
 
 end
