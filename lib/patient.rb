525122f1a763cb427eda5ad79dc678e9e310c87f
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 23:47:41 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,19 @@
 class Patient
   
-  attr_accessor
+  attr_accessor :doctor, :date, :name
+    
+  @@all = []
+    
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, doctor, self)
+    
+    
   
