0694cce3a34d24313c4bf202084ab25f786298e2
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 23:38:02 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Doctor
   
   attr_accessor :name
@@ -12,5 +14,18 @@ class Doctor
   def self.all
     @@all
   end
+  
+  def appointments
+    Appoinment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+  
+  def new_appointment(date, patient)
+    appointment = Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    binding.pry
   
 end
